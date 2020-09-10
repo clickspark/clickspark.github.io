b89926f0705fc88b239bbd8475f182d5c843b122
clickspark <49884892+clickspark@users.noreply.github.com>
2020-09-10 12:39:40 -0400

    committed

---
+++ precache-manifest.3abb723ec4044e41def0323646c0a8c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e68ce05fcf3b85a05a76a3b50d4a44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f84efbc617a553b11b5",
+    "url": "./static/css/main.55d9dbe7.chunk.css"
+  },
+  {
+    "revision": "f2b92057665828357f31",
+    "url": "./static/js/2.de468108.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.de468108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f84efbc617a553b11b5",
+    "url": "./static/js/main.c1d07537.chunk.js"
+  },
+  {
+    "revision": "602489c6930337ecfcf5",
+    "url": "./static/js/runtime-main.59f6ec1b.js"
+  },
+  {
+    "revision": "e9d5982fa6da51ecb0a4c34b387e42e1",
+    "url": "./static/media/space-bg.e9d5982f.jpg"
+  }
+]);
